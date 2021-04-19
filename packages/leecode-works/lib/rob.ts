c96036bde4eba4bec3839f633f56c81790aec560
Mistruth <kikizxc4@qq.com>
2021-04-19 09:47:25 +0800

    rob

--- packages/leecode-works/lib/rob.ts
+++ packages/leecode-works/lib/rob.ts
@@ -1,26 +1,27 @@
 export function rob(nums: number[]): number {
 
-  const dp: number[] = []
-  const dpWithoutFirst: number[] = [0]
+    const dp: number[] = []
+    const another: number[] = [0]
 
-  for(let i = 0; i < nums.length; i++) {
-
-    if(i >= 3) {
-      dpWithoutFirst[i] = Math.max(dpWithoutFirst[i - 2] + nums[i], dpWithoutFirst[i - 1])
+    for(let i = 0; i < nums.length; i++) {
       if(i === nums.length - 1) {
-        dp[i] = Math.max(dpWithoutFirst[i - 2] + nums[i], dp[i - 3] + nums[i - 1], dp[i - 1])
-      } else {
-        dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 3] + nums[i - 1], dp[i - 1])
+        dp[i] = Math.max(dp[i - 1], another[i - 2] + nums[i])
+        continue
       }
-    } else {
-      dp[i] = Math.max(...dp, nums[i])
-      if(i > 0) dpWithoutFirst[i] = Math.max(...dpWithoutFirst, nums[i])
+
+      if(i - 2 >= 0) {
+
+        dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1])
+        another[i] = Math.max(another[i - 2] + nums[i], another[i - 1])
+
+      } else {
+        dp[i] = Math.max(...dp, nums[i])
+        if(i > 0) {
+          another[i] = Math.max(...another, nums[i])
+        }
+      }
     }
-  }
 
-  console.log('dpWithoutFirst',dpWithoutFirst)
-  console.log('dp',dp)
-
-  return dp[dp.length - 1]
+    return dp[dp.length - 1]
 };
 
